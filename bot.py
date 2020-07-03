a2fb4c540e7e2b463d4ded71bbf6639b57cbdb5c
nocaed <codenamedeacon@outlook.com>
2020-07-03 18:56:33 -0400

    established basic connection

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext.commands import Bot
 
-# load in the discord client
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
-# run the client
-client.run(TOKEN)
+
+bot = Bot(command_prefix='rb!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected!')
+
+bot.run(TOKEN)
